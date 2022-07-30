bc178af0fa144013f48550da255e6173370ddb9d
woufs <897213449@qq.com>
2022-07-30 18:18:00 +0800

    添加项目文件。

---
+++ Project2/源.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[3] = { 1,2,3 };
+	int i = 0;
+	while (i < 3)
+	{
+		printf("%d\t", arr[i]);
+		i++;
+
+
+	}
+	return 0;
+}
